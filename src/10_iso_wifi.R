2caf331377b12db87e7cc55f2bb68ab5c84486e4
Teja Pristavec <tp2sk@virginia.edu>
2020-10-28 10:12:58 -0400

    Set up food retail isos

--- src/10_iso_wifi.R
+++ src/10_iso_wifi.R
@@ -88,12 +88,12 @@ options(osrm.server = "http://104.248.112.16:5000/", osrm.profile = "driving")
 # 
 # write_rds(wifi_10, "./data/working/wifi/wifi_10.rds")
 
-wifi_15 <- map_dfr(c(1:nrow(data)), ~osrmIsochrone(
-  loc = data[.x, ],
-  breaks = 15,
-  res = 200,
-  returnclass = "sf"
-))
-
-write_rds(wifi_15, "/home/tp2sk/Git/extension/data/working/wifi/wifi_15.rds")
+# wifi_15 <- map_dfr(c(1:nrow(data)), ~osrmIsochrone(
+#   loc = data[.x, ],
+#   breaks = 15,
+#   res = 200,
+#   returnclass = "sf"
+# ))
+# 
+# write_rds(wifi_15, "/home/tp2sk/Git/extension/data/working/wifi/wifi_15.rds")
 
---
+++ src/12_iso_foodretail.R
@@ -0,0 +1,111 @@
+library(readr)
+library(dplyr)
+library(osrm)
+library(sf)
+library(purrr)
+
+
+#
+# Read in ----------------------------------------------------------------------------
+#
+
+# The nonmissing DF
+data <- read_rds("/home/tp2sk/Git/extension/data/working/foodretail/foodretail_nonmiss.rds")
+
+# Split across 4 days
+data1 <- data[1:2009, ]
+data2 <- data[2010:4019, ]
+data3 <- data[4020:6029, ]
+data4 <- data[6030:8037, ]
+
+
+#
+# OSRM backend instance for Virginia locations on the SDAD server  -------------------------------
+#
+
+options(osrm.server = "http://104.248.112.16:5000/", osrm.profile = "driving")
+
+
+#
+# Get isochrones  --------------------------------------------------------------
+#
+
+# 10 minute isochrones PART 1
+foodretail_10_pt1 <- map_dfr(c(1:nrow(data1)), ~osrmIsochrone(
+  loc = data1[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_10_pt1, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_10_pt1.rds")
+
+# 10 minute isochrones PART 2
+foodretail_10_pt2 <- map_dfr(c(1:nrow(data2)), ~osrmIsochrone(
+  loc = data2[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_10_pt2, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_10_pt2.rds")
+
+# 10 minute isochrones PART 3
+foodretail_10_pt3 <- map_dfr(c(1:nrow(data3)), ~osrmIsochrone(
+  loc = data3[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_10_pt3, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_10_pt3.rds")
+
+# 10 minute isochrones PART 4
+foodretail_10_pt4 <- map_dfr(c(1:nrow(data4)), ~osrmIsochrone(
+  loc = data4[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_10_pt4, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_10_pt4.rds")
+
+# 15 minute isochrones PART 1
+foodretail_15_pt1 <- map_dfr(c(1:nrow(data1)), ~osrmIsochrone(
+  loc = data1[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_15_pt1, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_15_pt1.rds")
+
+# 15 minute isochrones PART 2
+foodretail_15_pt2 <- map_dfr(c(1:nrow(data2)), ~osrmIsochrone(
+  loc = data2[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_15_pt2, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_15_pt2.rds")
+
+# 15 minute isochrones PART 3
+foodretail_15_pt3 <- map_dfr(c(1:nrow(data3)), ~osrmIsochrone(
+  loc = data3[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_15_pt3, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_15_pt3.rds")
+
+# 15 minute isochrones PART 4
+foodretail_15_pt4 <- map_dfr(c(1:nrow(data4)), ~osrmIsochrone(
+  loc = data4[.x, ],
+  breaks = 10,
+  res = 200,
+  returnclass = "sf"
+))
+
+write_rds(foodretail_15_pt4, "/home/tp2sk/Git/extension/data/working/foodretail/foodretail_15_pt4.rds")
