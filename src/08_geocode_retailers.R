ee9df6fac4d6857c0dc62f4f68b8201cad432ab4
Teja Pristavec <tp2sk@virginia.edu>
2020-10-28 09:56:05 -0400

    Prepared retailer missing DF

--- src/08_geocode_retailers.R
+++ src/08_geocode_retailers.R
@@ -94,7 +94,16 @@ leaflet(store_geocoded) %>%
 # Write out --------------------------------------------------
 #
 
-write_rds(store_geocoded, "./data/working/foodretail/foodretailers.Rds")
+# All
+write_rds(store_geocoded, "./data/working/foodretail/foodretail_all.rds")
+
+# Nonmissing
+store_geocoded_nonmiss <- store_geocoded %>% filter(!is.na(longitude) & !is.na(latitude))
+write_rds(store_geocoded_nonmiss, "./data/working/foodretail/foodretail_nonmiss.rds")
+
+# Missing
+store_geocoded_miss <- store_geocoded %>% filter(is.na(longitude) | is.na(latitude))
+write_rds(store_geocoded_nonmiss, "./data/working/foodretail/foodretail_missing.rds")
 
 
 
