f334808aae099f192bc444afe4f8dd01b533a644
Morgan Stockham <morganstockham@gmail.com>
2020-11-19 13:59:16 -0500

    script for creating intersections in accomack for 8 minute ems isochrones

---
+++ src/14_isochrone_coverage.R
@@ -0,0 +1,36 @@
+library(sf)
+library(tidyverse)
+
+#
+# Coverage -------------------------------------------------
+#
+
+ems_points <- readRDS("./data/working/ems/final_ems.rds")
+
+rural <- read.csv("./data/original/srhp_rurality_2020/omb_srhp_rurality.csv")
+rural <- rural %>%
+  filter(RuralUrban == "R")
+rural$FIPS <- as.character(rural$FIPS)
+
+properties <- readRDS("./data/working/corelogic/final_corelogic.rds")
+properties <- properties %>%
+  filter(fips_code == "51001")
+
+ems_8 <- readRDS("./data/working/ems/final_ems_8.rds")
+ems_8 <- inner_join(ems_8, rural, by = c("geoid" = "FIPS"))
+ems_8 <- ems_8 %>%
+  filter(county == "Accomack")
+
+plot(st_geometry(properties))
+plot(st_geometry(ems_8))
+
+# Warning message:
+#   In st_is_longlat(x) :
+#   bounding box has potentially an invalid value range for longlat data
+#   The values of range seem inaccurate, and are certainly different than the range of accomack county
+
+for(i in 1:nrow(ems_8)){
+  int <- st_intersection(properties, ems_8[i,])
+  cov <- (nrow(int)/nrow(properties))*100
+  ems_8$coverage_8[i] <- cov
+}
